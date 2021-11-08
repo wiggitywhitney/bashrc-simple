78a4e00775d385d5a5f7df29f918612c4ea33637
leigh capili <leigh@null.net>
2021-11-08 15:52:20 -0700

    Gate direnv eval /w command -v

--- .bashrc.d/9_end/direnv.sh
+++ .bashrc.d/9_end/direnv.sh
@@ -1,2 +1,5 @@
-eval "$(direnv hook bash)"
-export DIRENV_LOG_FORMAT="$(printf "${fg_lblk}direnv: %%s${rst_txt}")"
+if command -v direnv >/dev/null
+then
+    eval "$(direnv hook bash)"
+    export DIRENV_LOG_FORMAT="$(printf "${fg_lblk}direnv: %%s${rst_txt}")"
+fi
